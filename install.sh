aa1f5cbb082c22983faaa653f8c62c27ec29ff09
Roger Qiu <roger.qiu@polycademy.com>
2016-12-12 19:26:39 +1100

    Added merging of windows temporary

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@
 # 
 # compile the m4 scripts and everything..
 # then install all the config files in the right places
+
+
+
+# Merge Windows User Temporary with Cygwin /tmp
+echo "none /tmp usertemp binary,posix=0 0 0" >> /etc/fstab
