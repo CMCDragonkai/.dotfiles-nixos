2f3bcc62ebe559e493b5e7703a957543297430d7
Roger Qiu <roger.qiu@polycademy.com>
2016-12-12 23:13:19 +1100

    Acquiring timezone information from the OS, for Linux and Cygwin

--- install.sh
+++ install.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
 
+# to be executed in after install.ps1
+# or directly.. if in Linux
+
 # install the python packages via pip2 and pip3 
 # this only occurs in Cygwin, not in Linux
 # 
 # compile the m4 scripts and everything..
 # then install all the config files in the right places
 
+# This occurs after packages are alreeady installed via cygwin packages
+# this needs to run the build...
+# but before we run the build we must interrogate a few things
+# and even run a few scripts
 
+if [[ "$(uname -s)" == Linux* ]]; then
 
-# Merge Windows User Temporary with Cygwin /tmp
-echo "none /tmp usertemp binary,posix=0 0 0" >> /etc/fstab
+    # on Linux, we assume timezone was already setup on OS installation
+    tz="$(cat /etc/zoneinfo)"
+    tzdir="/etc/zoneinfo"
+
+elif [[ $(uname -s) == CYGWIN* ]]; then
+
+    # Merge Windows User Temporary with Cygwin /tmp
+    echo "none /tmp usertemp binary,posix=0 0 0" >> /etc/fstab
+    
+    # Acquire timezone information from Windows
+    tz="$(./bin/tz-windows-to-iana "$(tzutil /l | grep --before-context=1 "$(tzutil /g)" | head --lines=1)")"
+    echo "$tz" > /etc/timezone
+    ln --symbolic --force /usr/share/zoneinfo/"$tz" /etc/localtime
+    ln --symbolic --force /usr/share/zoneinfo /etc/zoneinfo
+    tzdir="/usr/share/zoneinfo"
+
+fi
+
+# We need to pass "PH_TZ" and "PH_TZDIR" as variables to m4 preprocessor
