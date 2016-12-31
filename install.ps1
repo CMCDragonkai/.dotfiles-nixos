f8093ba51103e65b99d7fe38ea4a46fd0c64ae00
Roger Qiu <roger.qiu@polycademy.com>
2017-01-01 00:12:49 +1100

    WIP

--- install.ps1
+++ install.ps1
@@ -241,6 +241,34 @@ if ($Stage -eq 0) {
     # Rename the computer to the new name just before a restart
     Rename-Computer -NewName "$ComputerName" -Force >$null 2>&1
 
+    # Change power settings for the current plan
+    
+    # Power button hibernates the computer (Battery and Plugged In)
+    powercfg -setdcvalueindex SCHEME_CURRENT SUB_BUTTONS PBUTTONACTION 2
+    powercfg -setacvalueindex SCHEME_CURRENT SUB_BUTTONS PBUTTONACTION 2
+
+    # Lid close sleeps the computer (Battery and Plugged In)
+    powercfg -setdcvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
+    powercfg -setacvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
+
+    # Screen timing in seconds
+    # Turn off screen in 10 minutes on battery
+    # Turn off screen in 1 hr when plugged in
+    powercfg -setdcvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOIDLE (10 * 60)
+    powercfg -setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOIDLE (60 * 60)
+
+    # Sleep timing in seconds
+    # Sleep in 20 minutes on battery
+    # Never sleep when plugged in 
+    powercfg -setdcvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE (20 * 60)
+    powercfg -setacvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE 0
+
+    # Hibernate timing in seconds
+    # After sleeping for 2 hrs on battery, go to hibernation
+    # Never hibernate when plugged in
+    powercfg -setdcvalueindex SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE (120 * 60)
+    powercfg -setacvalueindex SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE 0
+
     # Schedule the next stage of this script and reboot
     Unregister-ScheduledTask -TaskName "Dotfiles - 1" -Confirm:$false -ErrorAction SilentlyContinue
     ScheduleRebootTask -Name "Dotfiles - " -Stage 1
