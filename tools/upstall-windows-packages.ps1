dabc910cd015d8c18be7e4cd9683673ebec5869f
Roger Qiu <roger.qiu@polycademy.com>
2017-01-06 18:07:48 +1100

    Update upstall-windows-packages.ps1

--- tools/upstall-windows-packages.ps1
+++ tools/upstall-windows-packages.ps1
@@ -6,36 +6,30 @@ param (
     [switch]$Force
 )
 
-Import-Module PackageManagement
-Import-Module PowerShellGet
-
-# Nuget is needed for PowerShellGet
+# PowerShellGet and PackageManagement are mandatory modules to bootstrap the rest of the installation
+# Install PowerShellGet from Nuget PackageProvider
 # Updating PowerShellGet updates PackageManagement
-# Update PackageManagement explicitly to be sure
-# PSReadline is nice
+# Update PackageManagement explicitly to be sure!
 if ($Force) {
     
     Install-PackageProvider -Name Nuget -Force
     Install-Module -Name PowerShellGet -Force
     Install-Module -Name PackageManagement -Force
-    Install-Module -Name PSReadline -Force -SkipPublisherCheck
-    Install-Module -Name BurntToast -Force
 
 } else {
 
     Install-PackageProvider -Name Nuget
     Install-Module -Name PowerShellGet
     Install-Module -Name PackageManagement
-    Install-Module -Name PSReadline -SkipPublisherCheck
-    Install-Module -Name BurntToast
-
+    
 }
 
 # Reimport PackageManagement and PowerShellGet to take advantage of the new commands
 Import-Module PackageManagement -Force
 Import-Module PowerShellGet -Force
 
-# The ChocolateyGet provider is more stable
+# Install extra package providers for chocolatey installation
+# The ChocolateyGet provider is just a thin wrapper around Chocolatey
 if ($Force) {
 
     Install-PackageProvider -Name 'ChocolateyGet' -Force
@@ -54,11 +48,47 @@ if ($Force) {
 
 # Make PowerShellGet's source trusted
 Set-PackageSource -Name 'PSGallery' -ProviderName 'PowerShellGet' -Trusted -Force
-    
+
 # Nuget doesn't register a package source by default
 Register-PackageSource -Name 'NuGet' -ProviderName 'NuGet' -Location 'https://www.nuget.org/api/v2' -Trusted -Force
 
-# Acquire the Package Lists
+# Acquire extra PowerShell Modules
+$PowerShellModules = (Get-Content "${PSScriptRoot}\..\windows_powershell_modules.txt" | Where-Object { 
+    $_.trim() -ne '' -and $_.trim() -notmatch '^#' 
+})
+
+# Install the PowerShell Modules
+foreach ($Module in $PowerShellModules) {
+
+    $Module = $Module -split ','
+    $Name = $Module[0].trim()
+    $Version = $Module[1].trim()
+    
+    if ($Module.Length -eq 3) {
+        $AdditionalArguments = $Module[2].trim()
+    } else {
+        $AdditionalArguments = ''
+    }
+
+    $InstallCommand = "Install-Module -Name '$Name' "
+
+    if ($Version -and $Version -ne '*') {
+        $InstallCommand += "-RequiredVersion '$Version' "
+    }
+
+    if ($AdditionalArguments) {
+        $InstallCommand += "$AdditionalArguments "
+    }
+
+    if ($Force) {
+        $InstallCommand += '-Force'
+    }
+
+    Invoke-Expression "$InstallCommand"
+
+}
+
+# Acquire the Windows Package List
 $WindowsPackages = (Get-Content "${PSScriptRoot}\..\windows_packages.txt" | Where-Object { 
     $_.trim() -ne '' -and $_.trim() -notmatch '^#' 
 })
