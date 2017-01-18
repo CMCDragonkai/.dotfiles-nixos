69c1c6abf28c9d6fc9bc0c91cdb98a494a2b7af0
Roger Qiu <roger.qiu@polycademy.com>
2017-01-18 23:26:04 +1100

    GDAL and boto for Python, using PSReadline, and AWS Default Config

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,22 +2,32 @@
 
 $host.ui.rawui.foregroundcolor = "white"
 
+Import-Module PSReadLine
+
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+Set-PSReadLineOption -MaximumHistoryCount 4096
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
 function prompt {
 
     $flag = $?
     $realLASTEXITCODE = $LASTEXITCODE
 
     # prepend a newline if the prior command did not terminate with a newline
-    if ($host.ui.rawui.cursorposition.x -eq 0) { 
+    if ($host.ui.rawui.cursorposition.x -eq 0) {
         $prompt_prefix = ""
     } else {
         $prompt_prefix = "`n"
     }
 
-    $status_prompt = if ($flag) { 
-        @{ prompt =" >"; color = "darkgreen" } 
-    } else { 
-        @{ prompt = " >" ; color = "red" } 
+    $status_prompt = if ($flag) {
+        @{ prompt =" >"; color = "darkgreen" }
+    } else {
+        @{ prompt = " >" ; color = "red" }
     }
 
     Write-Host -nonewline ($prompt_prefix + $env:username + " | " + $env:computername + " | " + (get-location) + "`n") -foregroundColor "cyan"
@@ -25,5 +35,5 @@ function prompt {
 
     $global:LASTEXITCODE = $realLASTEXITCODE
     return " "
-    
+
 }
