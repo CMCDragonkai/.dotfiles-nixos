a2a5bb71e2d064bf70afeb0416f96044597b6cec
Roger Qiu <roger.qiu@polycademy.com>
2017-02-04 03:42:46 +1100

    Understood the Windows Firewall Better, and how it applies to Profiles, Also turned on HTTP and HTTPS by default

--- .dotfiles-tools/set-firewall.ps1
+++ .dotfiles-tools/set-firewall.ps1
@@ -6,18 +6,81 @@ param (
     [string]$InstallationDirectory = "$Env:SystemDrive"
 )
 
-# Setup firewall to accept pings for Domain and Private networks but not from Public networks
+# We assume that there are 3 firewall profiles: Domain, Private and Public
+# Domain and Private will be considered trusted networks
+# We can expose networked services over these trusted networks
+# Public networks will be considered untrusted networks
+# We don't expose any critical network services on these untrusted networks
+# Note that unidentified networks are generally placed in public networks
+# Many virtual interfaces will default to un-indentified networks, and they will be placed into the public category
+# It's important to remember to set them to be private
+# Note that edge traversal policy is explain here: http://serverfault.com/q/89824/147813
+
+# Accept ICMP Pings for
 Set-NetFirewallRule `
     -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" `
     -Enabled True `
     -Action "Allow" `
-    -Profile "Domain,Private"`
+    -Profile "Domain,Private" `
     >$null
 Set-NetFirewallRule `
     -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" `
     -Enabled True `
     -Action "Allow" `
-    -Profile "Domain,Private"`
+    -Profile "Domain,Private" `
+    >$null
+
+# Port 445 for Samba File Sharing
+Remove-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-In)" -ErrorAction SilentlyContinue
+New-NetFirewallRule `
+    -DisplayName "File and Printer Sharing (SMB-In)" `
+    -Direction Inbound `
+    -EdgeTraversalPolicy Allow `
+    -Protocol TCP `
+    -LocalPort 445 `
+    -Action Allow `
+    -Profile "Domain,Private" `
+    -Enabled True `
+    >$null
+
+# Port 22 for Cygwin SSH
+Remove-NetFirewallRule -DisplayName "Polyhack - SSH (TCP-In)" -ErrorAction SilentlyContinue
+New-NetFirewallRule `
+    -DisplayName "Polyhack - SSH (TCP-In)" `
+    -Direction Inbound `
+    -EdgeTraversalPolicy Allow `
+    -Protocol TCP `
+    -LocalPort 22 `
+    -Action Allow `
+    -Profile "Domain,Private" `
+    -Program "${InstallationDirectory}\cygwin64\usr\sbin\sshd.exe" `
+    -Enabled True `
+    >$null
+
+# Port 80 for HTTP
+Remove-NetFirewallRule -DisplayName "Polyhack - HTTP (TCP-In)" -ErrorAction SilentlyContinue
+New-NetFirewallRule `
+    -DisplayName "Polyhack - HTTP (TCP-In)" `
+    -Direction Inbound `
+    -EdgeTraversalPolicy Allow `
+    -Protocol TCP `
+    -LocalPort 80 `
+    -Action Allow `
+    -Profile "Domain,Private" `
+    -Enabled True `
+    >$null
+
+# Port 443 for HTTP
+Remove-NetFirewallRule -DisplayName "Polyhack - HTTPS (TCP-In)" -ErrorAction SilentlyContinue
+New-NetFirewallRule `
+    -DisplayName "Polyhack - HTTPS (TCP-In)" `
+    -Direction Inbound `
+    -EdgeTraversalPolicy Allow `
+    -Protocol TCP `
+    -LocalPort 443 `
+    -Action Allow `
+    -Profile "Domain,Private" `
+    -Enabled True `
     >$null
 
 # Setup firewall to accept connections from 55555 in Domain and Private networks
@@ -41,31 +104,5 @@ New-NetFirewallRule `
     -LocalPort 55555 `
     -Action Allow `
     -Profile "Domain,Private" `
-    -Enabled True `
-    >$null
-
-# Port 22 for Cygwin SSH
-Remove-NetFirewallRule -DisplayName "Polyhack - SSH (TCP-In)" -ErrorAction SilentlyContinue
-New-NetFirewallRule `
-    -DisplayName "Polyhack - SSH (TCP-In)" `
-    -Direction Inbound `
-    -EdgeTraversalPolicy Allow `
-    -Protocol TCP `
-    -LocalPort 22 `
-    -Action Allow `
-    -Profile "Domain,Private" `
-    -Program "${InstallationDirectory}\cygwin64\usr\sbin\sshd.exe" `
-    -Enabled True `
-    >$null
-
-# Port 80 for HTTP, but blocked by default (switch it on when you need to)
-Remove-NetFirewallRule -DisplayName "Polyhack - HTTP (TCP-In)" -ErrorAction SilentlyContinue
-New-NetFirewallRule `
-    -DisplayName "Polyhack - HTTP (TCP-In)" `
-    -Direction Inbound `
-    -EdgeTraversalPolicy Allow `
-    -Protocol TCP `
-    -LocalPort 80 `
-    -Action Block `
     -Enabled True `
     >$null
