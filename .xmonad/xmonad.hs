ac5ccf5773ba998d4ca19acf0ceba50d63b43288
Roger Qiu <roger.qiu@polyhack.io>
2017-05-15 03:27:50 +1000

    Fleshing out xmonad desktop environment with xev, playerctl, and installed flightgear

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,43 +1,19 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
-{-
-
-    Furthermore, we have added `XMonad.Actions.ConstrainedReize` as `ConstrainedResize`, 
-    which allows us to do fixed aspect ratio resizing with `Mod + Shift + Button 3 Drag`. 
-    This module requires the `xmonad-contrib` package.
-
-    The `XMonad` namespace also brings in some operators like `<+>` and `.|.`. 
-    The `.|.` bitwise or operator will set the resultant bits to 1, if either of the 
-    initial bits is set to 1. I'm not sure how that translates to having 2 keys/buttons 
-    pressed at the same time. I do not know what the exact type of the `<+>` is, but 
-    I believe it merges the left and right functions of the same type, and those functions
-    return `Map` types, and it then merges the 2 `Map` types together. 
-    I found it, it's `Control.Arrow.(<+>)`, the type is `a b c -> a b c -> a b c`. It 
-    is a monoid append operation on arrows. The arrow `a b c` means `b -> c`. So it 
-    takes 2 arrows with the same input and output types, and returns an arrow with the 
-    same input and output. It's actually a generic function, so it's part of the `ArrowPlus` 
-    typeclass. XMonad must have created an instance for `ArrowPlus`, that's how this works!
-
-    The `NamedFieldPuns` extension allows us to pattern match on records using just 
-    their field names.
-
-    The `XMonad` namespace also brings in functions from a number of other modules too. 
-    Such as including `XMonad.ManageHook.composeAll`. And `=?` and `-->` and `doFloat` and 
-    `doShift`. However I'm actually using `Xmonad.Hooks.ManageHelpers.doCenterFloat`. Which 
-    is also a `xmonad-contrib` module.
-
--}
+import qualified Data.Map as Map
+import qualified XMonad.Actions.ConstrainedResize as ConstrainedResize
 
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Util.Run (spawnPipe, hPutStrLn)
-import XMonad.Hooks.DynamicLog
+
+import XMonad.Util.Run (safeSpawn, unsafeSpawn, spawnPipe, hPutStrLn)
+import XMonad.Util.EZConfig (additionalKeys)
+
 import XMonad.Hooks.ManageDocks (docks)
 import XMonad.Hooks.ManageHelpers (composeOne, doCenterFloat, (-?>))
-import XMonad.Util.EZConfig (additionalKeys)
-import qualified XMonad.Actions.ConstrainedResize as ConstrainedResize
 
-import qualified Data.Map as Map
+import Graphics.X11.ExtraTypes.XF86
+import XMonad.Hooks.DynamicLog
 
 matrixConfig = desktopConfig
     {
@@ -49,10 +25,78 @@ matrixConfig = desktopConfig
         mouseBindings = matrixMouse <+> mouseBindings desktopConfig,
         workspaces = [ "1", "2", "3" ]
     }
-    `additionalKeys`
-    [
-        ((mod4Mask .|. shiftMask, xK_z), spawn "i3lock && xset dpms force off")
-    ]
+    `additionalKeys` matrixKeys
+
+-- extra key actions
+-- screen lock and turn off screen
+-- screen shot active window
+-- screen shot entire screen
+-- music keys, volume down
+-- music keys, volume up
+matrixKeys =
+  [
+      (
+        (mod4Mask .|. shiftMask, xK_z),
+        unsafeSpawn "i3lock && xset dpms force off"
+      ),
+      (
+        (noModMask, xK_Print),
+        unsafeSpawn "gm import -quality 100 -window $(xdotool getwindowfocus -f) ~/Pictures/window_$(date +%s).png"
+      ),
+      (
+        (mod4Mask, xK_Print),
+        unsafeSpawn "gm import -quality 100 -window root ~/Pictures/screenshot_$(date +%s).png"
+      ),
+      (
+        (noModMask, xF86XK_AudioPlay),
+        unsafeSpawn "playerctl play"
+      ),
+      (
+        (noModMask, xF86XK_AudioPause),
+        unsafeSpawn "playerctl pause"
+      ),
+      (
+        (noModMask, xF86XK_AudioStop),
+        unsafeSpawn "playerctl stop"
+      ),
+      (
+        (noModMask, xF86XK_AudioPrev),
+        unsafeSpawn "playerctl previous"
+      ),
+      (
+        (noModMask, xF86XK_AudioNext),
+        unsafeSpawn "playerctl next"
+      ),
+      (
+        (noModMask, xF86XK_AudioMute),
+        unsafeSpawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+      ),
+      (
+        (noModMask, xF86XK_AudioLowerVolume),
+        unsafeSpawn "pactl set-sink-volume @DEFAULT_SINK@ -10%"
+      ),
+      (
+
+        (noModMask, xF86XK_AudioRaiseVolume),
+        unsafeSpawn "pactl set-sink-volume @DEFAULT_SINK@ +10%"
+      ),
+      (
+        (noModMask, xF86XK_MonBrightnessDown),
+        unsafeSpawn
+          "read -r level </sys/class/backlight/acpi_video0/brightness\n\
+          \read -r max </sys/class/backlight/acpi_video0/max_brightness\n\
+          \factor=$((100/max))\n\
+          \xbacklight -set \"$((level * factor))\""
+      ),
+      (
+        (noModMask, xF86XK_MonBrightnessUp),
+        unsafeSpawn
+          "read -r level </sys/class/backlight/acpi_video0/brightness\n\
+          \read -r max </sys/class/backlight/acpi_video0/max_brightness\n\
+          \factor=$((100/max))\n\
+          \xbacklight -set \"$((level * factor))\""
+      )
+  ]
 
 -- actions to perform for specific window classes
 matrixHooks =
