6f8c1f6dbecb1a131004040c1ed85545fb00e877
Roger Qiu <roger.qiu@polyhack.io>
2018-09-19 22:21:30 +1000

    Partitioned config.nix

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -7,251 +7,305 @@
   };
   packageOverrides = pkgs:
     with pkgs;
-    {
-      env-all = buildEnv {
-        name = "env-all";
-        paths = [
-          # Fonts
-          fira
-          fira-mono
-          fira-code
-          dejavu_fonts
-          # Web
-          firefox
-          chromium
-          # Email
-          thunderbird
-          # Data Science
-          schemaspy
-          dbeaver
-          sqlite-interactive
-          sqlite3_analyzer
-          sc-im
-          gnuplot
-          jq
-          python36Packages.csvkit
-          basex
-          libxml2
-          go-pup
-          gdal
-          proj
-          qgis
-          hdf5
-          hdfview
-          # Documents and Graphs
-          libreoffice
-          zathura
-          ghostscriptX
-          poppler_utils
-          pdftk
-          pandoc
-          plantuml
-          graphviz
-          dia
-          aspell
-          aspellDicts.en
-          # masterpdfeditor
-          # System Management & Monitoring
-          lnav
-          smem
-          ncdu
-          htop
-          atop
-          iftop
-          iptraf
-          iotop
-          conky
-          python36Packages.glances
-          stress-ng
-          powertop
-          # Keyboard
-          teensy-loader-cli
-          # Math
-          R
-          bc
-          # Shell and Environment Utilities
-          file
-          findutils
-          redshift
-          i3lock
-          tree
-          parallel
-          asciinema
-          reptyr
-          lsof
-          rmlint
-          pv
-          cv
-          proot
-          lsof
-          picocom
-          kdeApplications.konsole
-          gnum4
-          ag
-          dex
-          hicolor_icon_theme
-          numix-gtk-theme
-          gnome3.nautilus
-          gnome3.dconf
-          rlwrap
-          trash-cli
-          inotify-tools
-          synergy
-          # Security
-          plasma5.polkit-kde-agent
-          oathToolkit
-          certbot
-          keepass
-          keybase
-          pcsctools
-          paperkey
-          # Archive Management
-          zip
-          unzip
-          gzip
-          bzip2
-          lzma
-          dpkg
-          # Network
-          mitmproxy
-          openssl
-          putty
-          wget
-          httpie
-          aria
-          mailutils
-          bind
-          ldns
-          ipfs
-          nodePackages_8_x.dat
-          rsync
-          wireshark
-          nmap
-          ncat
-          socat
-          hans
-          iodine
-          udptunnel
-          httptunnel
-          pwnat
-          ngrok
-          sslh
-          geoipWithDatabase
-          iperf
-          conntrack_tools
-          nethogs
-          openvpn
-          filezilla
-          # Storage
-          nfs-utils
-          samba4
-          netatalk
-          dropbox-cli
-          awscli
-          unetbootin
-          # Nix
-          nix-repl
-          nix-prefetch-scripts
-          nix-index
-          nix-bundle
-          vulnix
-          # Development
-          emacs
-          vimHugeX
-          tmux
-          binutils
-          patchelf
-          dos2unix
-          qemu
-          vagrant
-          shellcheck
-          universal-ctags
-          global
-          python36Packages.binwalk-full
-          python36Packages.pygments
-          man-pages
-          posix_man_pages
-          libcap_manpages
-          valgrind
-          gdb
-          lldb
-          rr
-          docker_compose
-          runc
-          # Android
-          libmtp
-          go-mtpfs
-          apktool
-          python36Packages.gplaycli
-          # IDE dependencies
-          (
-            with python36Packages;
-            python.buildEnv.override {
-              extraLibs = [
-                setuptools
-                jedi
-                flake8
-                isort
-                yapf
-                pytest
-              ];
-            }
-          )
-          nodePackages.tern
-          nodePackages.js-beautify
-          rustracer
-          go
-          gocode
-          godef
-          haskellPackages.stylish-haskell
-          haskellPackages.pretty-show
-          # Chat
-          weechat
-          # skype
-          slack
-          # X Window and XMonad
-          xdg_utils
-          xdg-user-dirs
-          dmenu
-          xorg.xev
-          xorg.xmessage
-          xorg.xwininfo
-          xorg.xdpyinfo
-          xdotool
-          xclip
-          hsetroot
-          haskellPackages.xmobar
-          autorandr
-          # Media
-          pavucontrol
-          kdeApplications.spectacle
-          vlc
-          mplayer
-          playerctl
-          spotify
-          beets
-          ffcast
-          ffmpeg
-          feh
-          sox
-          gimp
-          graphicsmagick
-          imagemagick
-          libtiff
-          v4l_utils
-          mediainfo
-          python36Packages.youtube-dl
-          perlPackages.ImageExifTool
-          dcraw
-          zbar
-          # Radio
-          gnuradio-with-packages
-          gqrx
-          # Gaming
-          steam
-          # Finance
-          electrum
-        ];
+    let
+      pkgsMaster = import
+        (
+          builtins.fetchTarball
+          https://github.com/NixOS/nixpkgs/archive/master.tar.gz
+        ) {};
+    in
+      {
+        # all qt applications have to be part of the same buildEnv
+        # they rely on shared libraries that is shared across all packages
+        env = {
+          base = buildEnv {
+            name = "env-base";
+            paths = [
+              # Fonts
+              fira
+              fira-mono
+              fira-code
+              dejavu_fonts
+              # Web
+              firefox
+              chromium
+              # Email
+              thunderbird
+              # System Management & Monitoring
+              lnav
+              smem
+              ncdu
+              htop
+              atop
+              iftop
+              iptraf
+              nethogs
+              iotop
+              powertop
+              stress-ng
+              fio
+              conky
+              python36Packages.glances
+              # Keyboard
+              teensy-loader-cli
+              # Math
+              R
+              bc
+              # Shell and Environment Utilities
+              file
+              findutils
+              redshift
+              i3lock
+              tree
+              parallel
+              asciinema
+              reptyr
+              lsof
+              rmlint
+              pv
+              cv
+              proot
+              lsof
+              picocom
+              kdeApplications.konsole
+              gnum4
+              ag
+              dex
+              hicolor_icon_theme
+              numix-gtk-theme
+              gnome3.nautilus
+              gnome3.dconf
+              rlwrap
+              trash-cli
+              inotify-tools
+              # Security
+              plasma5.polkit-kde-agent
+              oathToolkit
+              certbot
+              keepass
+              keybase
+              pcsctools
+              paperkey
+              # Archive Management
+              zip
+              unzip
+              gzip
+              bzip2
+              lzma
+              dpkg
+              # Network
+              mitmproxy
+              openssl
+              putty
+              wget
+              httpie
+              aria
+              mailutils
+              bind
+              ldns
+              ipfs
+              nodePackages_8_x.dat
+              rsync
+              wireshark
+              nmap
+              ncat
+              socat
+              hans
+              iodine
+              udptunnel
+              httptunnel
+              pwnat
+              ngrok
+              sslh
+              geoipWithDatabase
+              iperf
+              conntrack_tools
+              openvpn
+              # Storage
+              filezilla
+              nfs-utils
+              samba4
+              netatalk
+              awscli
+              unetbootin
+              # Nix
+              nix-repl
+              nix-prefetch-scripts
+              nix-index
+              nix-bundle
+              vulnix
+              # Chat
+              weechat
+              # X Window and XMonad
+              xdg_utils
+              xdg-user-dirs
+              dmenu
+              xorg.xev
+              xorg.xmessage
+              xorg.xwininfo
+              xorg.xdpyinfo
+              xdotool
+              xclip
+              hsetroot
+              haskellPackages.xmobar
+              feh
+              autorandr
+              # Media
+              pavucontrol
+              kdeApplications.spectacle
+              playerctl
+              v4l_utils
+              vlc
+              ffmpeg
+              sox
+              mplayer
+              beets
+              ffcast
+              mediainfo
+              python36Packages.youtube-dl
+              electrum
+              # Radio
+              gnuradio-with-packages
+              gqrx
+            ];
+          };
+          documents = buildEnv {
+            name = "env-documents";
+            paths = [
+              libreoffice
+              zathura
+              ghostscriptX
+              poppler_utils
+              pdftk
+              pandoc
+              aspell
+              aspellDicts.en
+            ];
+          };
+          development = buildEnv {
+            name = "env-development";
+            paths = [
+              # Development
+              emacs
+              vimHugeX
+              tmux
+              binutils
+              patchelf
+              dos2unix
+              qemu
+              vagrant
+              shellcheck
+              universal-ctags
+              global
+              python36Packages.binwalk-full
+              python36Packages.pygments
+              man-pages
+              posix_man_pages
+              libcap_manpages
+              valgrind
+              gdb
+              lldb
+              rr
+              docker_compose
+              runc
+              # IDE dependencies
+              (
+                with python36Packages;
+                python.buildEnv.override {
+                  extraLibs = [
+                    setuptools
+                    jedi
+                    flake8
+                    isort
+                    yapf
+                    pytest
+                  ];
+                }
+              )
+              nodePackages.tern
+              nodePackages.js-beautify
+              rustracer
+              go
+              gocode
+              godef
+              haskellPackages.stylish-haskell
+              haskellPackages.pretty-show
+            ];
+          };
+          graphics = buildEnv {
+            name = "env-graphics";
+            paths = [
+              dcraw
+              graphicsmagick
+              imagemagick
+              perlPackages.ImageExifTool
+              gimp
+              libtiff
+            ];
+          };
+          graphs = buildEnv {
+            name = "env-graphs";
+            paths = [
+              plantuml
+              graphviz
+              gnuplot
+              dia
+            ];
+          };
+          data = buildEnv {
+            name = "env-data";
+            paths = [
+              jq                      # json
+              libxml2                 # xml
+              python36Packages.csvkit # csv
+              hdf5                    # hdf5
+              hdfview
+              go-pup                  # html
+            ];
+          };
+          sql = buildEnv {
+            name = "env-sql";
+            paths = [
+              schemaspy
+              dbeaver
+              sqlite-interactive
+              sqlite3_analyzer
+            ];
+          };
+          gis = buildEnv {
+            name = "env-gis";
+            paths = [
+              gdal
+              proj
+              qgis
+            ];
+          };
+          gaming = buildEnv {
+            name = "env-gaming";
+            paths = [
+              steam
+            ];
+          };
+          android = buildEnv {
+            name = "env-android";
+            paths = [
+              libmtp
+              go-mtpfs
+              apktool
+              python36Packages.gplaycli
+            ];
+          };
+          # proprietary applications tend to expire quickly
+          # so we need to follow master for these
+          proprietary = with pkgsMaster; buildEnv {
+            name = "env-proprietary";
+            paths = [
+              masterpdfeditor
+              skype
+              slack
+              spotify
+            ];
+          };
+        };
       };
-    };
 }
