7560a801ff1c93284d0115492e828d7c52533fc7
Roger Qiu <roger.qiu@polyhack.io>
2018-09-19 21:24:12 +1000

    Added autorandr and reformatted the config.nix

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,258 +1,257 @@
 {
-  packageOverrides = superPkgs:
-  with superPkgs;
-  {
-    # python environment with python packages
-    # used for IDE integration & experiments
-    env-all = with pkgs; buildEnv {
-      name = "env-all";
-      paths = [
-        # Fonts
-        fira
-        fira-mono
-        fira-code
-        dejavu_fonts
-        # Web
-        firefox
-        chromium
-        # Email
-        thunderbird
-        # Data Science
-        schemaspy
-        dbeaver
-        sqlite-interactive
-        sqlite3_analyzer
-        sc-im
-        gnuplot
-        jq
-        python36Packages.csvkit
-        basex
-        libxml2
-        go-pup
-        gdal
-        proj
-        qgis
-        hdf5
-        hdfview
-        # Documents and Graphs
-        libreoffice
-        zathura
-        ghostscriptX
-        poppler_utils
-        pdftk
-        pandoc
-        plantuml
-        graphviz
-        dia
-        aspell
-        aspellDicts.en
-        # masterpdfeditor
-        # System Management & Monitoring
-        lnav
-        smem
-        ncdu
-        htop
-        atop
-        iftop
-        iptraf
-        iotop
-        conky
-        python36Packages.glances
-        stress-ng
-        # Keyboard
-        teensy-loader-cli
-        # Math
-        R
-        bc
-        # Shell and Environment Utilities
-        file
-        findutils
-        redshift
-        i3lock
-        tree
-        parallel
-        asciinema
-        reptyr
-        lsof
-        rmlint
-        pv
-        cv
-        proot
-        lsof
-        picocom
-        kdeApplications.konsole
-        gnum4
-        ag
-        dex
-        hicolor_icon_theme
-        numix-gtk-theme
-        gnome3.nautilus
-        gnome3.dconf
-        rlwrap
-        trash-cli
-        inotify-tools
-        synergy
-        # Security
-        plasma5.polkit-kde-agent
-        oathToolkit
-        certbot
-        keepass
-        keybase
-        pcsctools
-        paperkey
-        # Archive Management
-        zip
-        unzip
-        gzip
-        bzip2
-        lzma
-        dpkg
-        # Network
-        mitmproxy
-        openssl
-        putty
-        wget
-        httpie
-        aria
-        mailutils
-        bind
-        ldns
-        ipfs
-        nodePackages_8_x.dat
-        rsync
-        wireshark
-        nmap
-        ncat
-        socat
-        hans
-        iodine
-        udptunnel
-        httptunnel
-        pwnat
-        ngrok
-        sslh
-        geoipWithDatabase
-        iperf
-        conntrack_tools
-        nethogs
-        openvpn
-        filezilla
-        # Storage
-        nfs-utils
-        samba4
-        netatalk
-        dropbox-cli
-        awscli
-        unetbootin
-        # Nix
-        nix-repl
-        nix-prefetch-scripts
-        nix-index
-        nix-bundle
-        vulnix
-        # Development
-        emacs
-        vimHugeX
-        tmux
-        binutils
-        patchelf
-        dos2unix
-        qemu
-        vagrant
-        shellcheck
-        universal-ctags
-        global
-        python36Packages.binwalk-full
-        python36Packages.pygments
-        man-pages
-        posix_man_pages
-        libcap_manpages
-        valgrind
-        gdb
-        lldb
-        rr
-        docker_compose
-        runc
-        # Android
-        libmtp
-        go-mtpfs
-        apktool
-        python36Packages.gplaycli
-        # IDE dependencies
-        (
-          with python36Packages;
-          python.buildEnv.override {
-            extraLibs = [
-              setuptools
-              jedi
-              flake8
-              isort
-              yapf
-              pytest
-            ];
-          }
-        )
-        nodePackages.tern
-        nodePackages.js-beautify
-        rustracer
-        go
-        gocode
-        godef
-        haskellPackages.stylish-haskell
-        haskellPackages.pretty-show
-        # Chat
-        weechat
-        # skype
-        slack
-        # X Window and XMonad
-        xdg_utils
-        xdg-user-dirs
-        dmenu
-        xorg.xev
-        xorg.xmessage
-        xorg.xwininfo
-        xorg.xdpyinfo
-        xdotool
-        xclip
-        hsetroot
-        haskellPackages.xmobar
-        # Media
-        pavucontrol
-        kdeApplications.spectacle
-        vlc
-        mplayer
-        playerctl
-        spotify
-        beets
-        ffcast
-        ffmpeg
-        feh
-        sox
-        gimp
-        graphicsmagick
-        imagemagick
-        libtiff
-        v4l_utils
-        mediainfo
-        python36Packages.youtube-dl
-        perlPackages.ImageExifTool
-        dcraw
-        zbar
-        # Radio
-        gnuradio-with-packages
-        gqrx
-        # Gaming
-        steam
-        flightgear
-        # Finance
-        electrum
-      ];
-    };
-  };
   allowUnfree = true;
   pulseaudio = true;
   chromium = {
     pulseSupport = true;
     enableNaCl = true;
   };
+  packageOverrides = pkgs:
+    with pkgs;
+    {
+      env-all = buildEnv {
+        name = "env-all";
+        paths = [
+          # Fonts
+          fira
+          fira-mono
+          fira-code
+          dejavu_fonts
+          # Web
+          firefox
+          chromium
+          # Email
+          thunderbird
+          # Data Science
+          schemaspy
+          dbeaver
+          sqlite-interactive
+          sqlite3_analyzer
+          sc-im
+          gnuplot
+          jq
+          python36Packages.csvkit
+          basex
+          libxml2
+          go-pup
+          gdal
+          proj
+          qgis
+          hdf5
+          hdfview
+          # Documents and Graphs
+          libreoffice
+          zathura
+          ghostscriptX
+          poppler_utils
+          pdftk
+          pandoc
+          plantuml
+          graphviz
+          dia
+          aspell
+          aspellDicts.en
+          # masterpdfeditor
+          # System Management & Monitoring
+          lnav
+          smem
+          ncdu
+          htop
+          atop
+          iftop
+          iptraf
+          iotop
+          conky
+          python36Packages.glances
+          stress-ng
+          powertop
+          # Keyboard
+          teensy-loader-cli
+          # Math
+          R
+          bc
+          # Shell and Environment Utilities
+          file
+          findutils
+          redshift
+          i3lock
+          tree
+          parallel
+          asciinema
+          reptyr
+          lsof
+          rmlint
+          pv
+          cv
+          proot
+          lsof
+          picocom
+          kdeApplications.konsole
+          gnum4
+          ag
+          dex
+          hicolor_icon_theme
+          numix-gtk-theme
+          gnome3.nautilus
+          gnome3.dconf
+          rlwrap
+          trash-cli
+          inotify-tools
+          synergy
+          # Security
+          plasma5.polkit-kde-agent
+          oathToolkit
+          certbot
+          keepass
+          keybase
+          pcsctools
+          paperkey
+          # Archive Management
+          zip
+          unzip
+          gzip
+          bzip2
+          lzma
+          dpkg
+          # Network
+          mitmproxy
+          openssl
+          putty
+          wget
+          httpie
+          aria
+          mailutils
+          bind
+          ldns
+          ipfs
+          nodePackages_8_x.dat
+          rsync
+          wireshark
+          nmap
+          ncat
+          socat
+          hans
+          iodine
+          udptunnel
+          httptunnel
+          pwnat
+          ngrok
+          sslh
+          geoipWithDatabase
+          iperf
+          conntrack_tools
+          nethogs
+          openvpn
+          filezilla
+          # Storage
+          nfs-utils
+          samba4
+          netatalk
+          dropbox-cli
+          awscli
+          unetbootin
+          # Nix
+          nix-repl
+          nix-prefetch-scripts
+          nix-index
+          nix-bundle
+          vulnix
+          # Development
+          emacs
+          vimHugeX
+          tmux
+          binutils
+          patchelf
+          dos2unix
+          qemu
+          vagrant
+          shellcheck
+          universal-ctags
+          global
+          python36Packages.binwalk-full
+          python36Packages.pygments
+          man-pages
+          posix_man_pages
+          libcap_manpages
+          valgrind
+          gdb
+          lldb
+          rr
+          docker_compose
+          runc
+          # Android
+          libmtp
+          go-mtpfs
+          apktool
+          python36Packages.gplaycli
+          # IDE dependencies
+          (
+            with python36Packages;
+            python.buildEnv.override {
+              extraLibs = [
+                setuptools
+                jedi
+                flake8
+                isort
+                yapf
+                pytest
+              ];
+            }
+          )
+          nodePackages.tern
+          nodePackages.js-beautify
+          rustracer
+          go
+          gocode
+          godef
+          haskellPackages.stylish-haskell
+          haskellPackages.pretty-show
+          # Chat
+          weechat
+          # skype
+          slack
+          # X Window and XMonad
+          xdg_utils
+          xdg-user-dirs
+          dmenu
+          xorg.xev
+          xorg.xmessage
+          xorg.xwininfo
+          xorg.xdpyinfo
+          xdotool
+          xclip
+          hsetroot
+          haskellPackages.xmobar
+          autorandr
+          # Media
+          pavucontrol
+          kdeApplications.spectacle
+          vlc
+          mplayer
+          playerctl
+          spotify
+          beets
+          ffcast
+          ffmpeg
+          feh
+          sox
+          gimp
+          graphicsmagick
+          imagemagick
+          libtiff
+          v4l_utils
+          mediainfo
+          python36Packages.youtube-dl
+          perlPackages.ImageExifTool
+          dcraw
+          zbar
+          # Radio
+          gnuradio-with-packages
+          gqrx
+          # Gaming
+          steam
+          # Finance
+          electrum
+        ];
+      };
+    };
 }
