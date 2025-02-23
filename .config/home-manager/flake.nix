20b9f308652c13b0bc314dc2821bf189590e4d88
Roger Qiu <roger.qiu@polyhack.io>
2025-02-22 23:12:07 -0600

    feat: home-manager flake attempt

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs-matrix-private = {
+      type = "indirect";
+      id = "nixpkgs-matrix-private";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-matrix-private/nixpkgs-matrix/nixpkgs";
+    };
+    polykey.url = "github:MatrixAI/Polykey-CLI";
+  };
+  outputs = { nixpkgs-matrix-private, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      username = "cmcdragonkai";
+    in {
+      homeConfigurations.${username} =
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          extraSpecialArgs = { inherit inputs system username; };
+          modules = [ ./home.nix ];
+        };
+    };
+}
---
+++ .config/home-manager/home.nix
@@ -0,0 +1,22 @@
+{ pkgs, username, inputs, ... }:
+
+{
+  imports = [ inputs.polykey-cli.homeModules.default ];
+  home = {
+    inherit username;
+    homeDirectory = "/home/${username}";
+    stateVersion = "25.05";
+    packages = with pkgs; [
+      vscode 
+      discord
+    ];
+    programs = {
+      home-manager.enable = true;
+      polykey = {
+        enable = true;
+        passwordFilePath = "%h/.polykeypass";
+        recoveryCodeOutPath = "%h/.polykeyrecovery";
+      };
+    };
+  };
+}
